b877f17da9808a8eafd3ab58d9ad6675853a6fb2
fdrive422 <fafrago@gmail.com>
2023-04-27 20:58:31 -0700

    homepage design and logic

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,25 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
+"use client";
 
-const inter = Inter({ subsets: ["latin"] });
+import CitySelector from "@/components/CitySelector";
+import { Card, Divider, Subtitle, Text } from "@tremor/react";
 
 export default function Home() {
 	return (
-		<main className="text-2xl">
-			<h1>Lets build a GPT Weather App</h1>
-		</main>
+		<div className="min-h-screen bg-gradient-to-br from-[#394f68] to-[#183B7E] p-10 flex flex-col justify-center items-center">
+			<Card className="max-w-4xl mx-auto">
+				<Text className="text-6xl font-bold text-center mb-10">
+					Weather AI
+				</Text>
+				<Subtitle className="text-xl text-center">
+					Powered by OpenAI, Next.js 13.3, Tailwind CSS, Termor 2.0 +
+					More!
+				</Subtitle>
+
+				<Divider className="my-10" />
+				<Card className="bg-gradient-to-br from-[#394f68] to-[#183B7E]">
+					<CitySelector />
+				</Card>
+			</Card>
+		</div>
 	);
 }
