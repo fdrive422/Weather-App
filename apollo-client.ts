88cd1b94b6c318e73b80ff4ba88698fb7785876d
fdrive422 <fafrago@gmail.com>
2023-04-28 14:18:27 -0700

    setup graphql backend api endpoint

---
+++ apollo-client.ts
@@ -0,0 +1,13 @@
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+
+let client: ApolloClient<any> | null = null;
+
+export const getClient = () => {
+	const client = new ApolloClient({
+		uri: process.env.API_URL,
+		cache: new InMemoryCache(),
+		headers: {
+			Authorization: `apikey ${process.env.NEXT_PUBLIC_STEPZEN_API_KEY}`
+		},
+	});
+};
--- components/CitySelector.tsx
+++ components/CitySelector.tsx
@@ -47,9 +47,9 @@ function CitySelector() {
 
 	const handleSelectedCity = (option: cityOption) => {
 		setSelectedCity(option);
-		// router.push(
-		// 	`/location/${option?.value.latitude}/${option?.value.longitude}`
-		// );
+		router.push(
+			`/location/${option?.value.name}/${option?.value.latitude}/${option?.value.longitude}`
+		);
 	};
 	return (
 		<div className="space-y-4">
