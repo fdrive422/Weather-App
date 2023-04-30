3ed53d767abed7ef527ac0ec2c25c1e5e275e02b
fdrive422 <fafrago@gmail.com>
2023-04-30 16:49:17 -0700

    fix prop types for calloutcard

--- app/location/[city]/[lat]/[long]/page.tsx
+++ app/location/[city]/[lat]/[long]/page.tsx
@@ -1,3 +1,7 @@
+import { getClient } from "@/apollo-client";
+import CalloutCard from "@/components/CalloutCard";
+import fetchWeatherQuery from "@/graphql/queries/fetchWeatherQueries";
+
 type Props = {
 	params: {
 		city: string;
@@ -6,10 +10,44 @@ type Props = {
 	};
 };
 
-function WeatherPage({ params: { city, lat, long } }: Props) {
+async function WeatherPage({ params: { city, lat, long } }: Props) {
+	const client = getClient();
+
+	const { data } = await client.query({
+		query: fetchWeatherQuery,
+		variables: {
+			current_weather: "true",
+			longitude: long,
+			latitude: lat,
+			timezone: "PST",
+		},
+	});
+
+	const results: Root = data.myQuery;
+
+	console.log(results);
+
 	return (
 		<div>
-			Welcome to the weather page: {city} {lat} {long}{" "}
+			{/* InformationPanel */}
+			<div>
+				<div className="p-5">
+					<div className="pb-5">
+						<h2 className="text-xl font-bold">Todays Overview</h2>
+						<p className="text-sm text-gray-400">
+							Last Updated at:{" "}
+							{new Date(
+								results.current_weather.time
+							).toLocaleString()}{" "}
+							({results.timezone})
+						</p>
+					</div>
+
+					<div>
+						<CalloutCard message="This is where GPT-4 Summary will go!" />
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
