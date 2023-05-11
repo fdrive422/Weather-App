9d5147b33fcc461b686c688d4fb683c9fdb782b6
fdrive422 <fafrago@gmail.com>
2023-05-10 21:38:13 -0700

    slector screen font size

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
 	return (
 		<div className="min-h-screen bg-gradient-to-br from-[#394f68] to-[#183B7E] p-10 flex flex-col justify-center items-center">
 			<Card className="max-w-4xl mx-auto">
-				<Text className="text-6xl font-bold text-center mb-10">
+				<Text className="text-5xl font-bold text-center mb-10">
 					Weather AI
 				</Text>
 				<Subtitle>
