027eb4fd082c9ddec64ece16187680283d23069a
fdrive422 <fafrago@gmail.com>
2023-05-11 18:59:52 -0700

    load screen font

--- app/location/[city]/[lat]/[long]/loading.tsx
+++ app/location/[city]/[lat]/[long]/loading.tsx
@@ -7,7 +7,7 @@ function Loading() {
 				className="h-24 w-24 animate-bounce text-yellow-500"
 				color="yellow"
 			/>
-			<h1 className="text-6xl font-bold text-center mb-10 animate-pulse">
+			<h1 className="text-5xl font-bold text-center mb-10 animate-pulse">
 				Loading City Weather Information
 			</h1>
 			<h2 className="text-xl font-bold text-center mb-10 animate-pulse">
